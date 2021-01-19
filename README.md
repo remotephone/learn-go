bbbd18040a51fce91cb5d289730c7af4a1b86ee5
remotephone@gmail.com <remotephone@gmail.com>
2021-01-18 22:48:07 -0600

    moving and reorganizing

---
+++ README.md
@@ -0,0 +1,12 @@
+# Learning Go
+
+This should be useful. This is a collection of notes a demos and stuff I am doing while learning this language. 
+
+## Go: The Complete Developer's Guide (Golang)
+
+I am working through `Go: The complete developer's guide (Golang)` on Udemy. It seems like good material. The content in the `go_complete_developers_guide` directory comes from that course. 
+
+https://udemy.com/course/go-the-complete-developers-guide/
+
+## Random
+Just random files and scripts to try things out. 
---
+++ go_complete_developers_guide/cards/deck.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// Create a new type 'deck'
+// which is a slice of strings
+
+type deck []string
+
+// we did not add a receiver to this function because 
+// we don't have a deck we're already working with
+func newDeck() deck  {
+	// create new variable of type deck that is empty
+	// We could simply write out all 52 cards, but thats tedious
+	cards := deck{}
+	// Let's create two lists, one of suits, one of values
+	// use two for loops to iterate through them and generate an entire deck!
+	// for each suit in cardSuits
+	//    for each value in cardValues
+	//        Add a new card of 'value of suit' to 'cards' deck
+	
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+
+	// Iterate through both lists
+	// The _ tells go we know theres a variable here but we're not gonna use it
+	// Index, Value
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d { // for index value, card in range cards
+		fmt.Println(i, card)	// print index number and card
+	}
+}
