bd29420048299a78b660cb0355f828ecaadcb4f6
Jaime Flores <jflores@indeed.com>
2021-05-28 20:55:04 -0500

    more stuff on structs

--- go_complete_developers_guide/structs/oldmain.go
+++ go_complete_developers_guide/structs/oldmain.go
@@ -19,7 +19,7 @@ type person2 struct {
 }
 
 
-func main() {
+func main1() {
 	// first define all the properties it might have
 	// create a ruleset and you can create a value
 	// Let's create a person struct. First name, last name
@@ -27,12 +27,42 @@ func main() {
 	// Kinda wacky, relies on positional arguments, if you swap anything you break stuff
 	alex := person{"Alex", "Anderson"}
 
+	// more reasonable
+	tim := person{firstName: "Tim", lastName: "Tomas"}
 
+	// assigns zero value to each field
+	var steve person
+
+	fmt.Println(alex)
+	fmt.Println(tim)
+	fmt.Println(steve)
+	fmt.Printf("%+v", steve)
+
+	alex.firstName = "Alexson"
+	fmt.Println(alex)
+
+	// Go will assign zero value to these struct values
+	var jim person
+	fmt.Printf("%+v", jim)
 	alex.print()
-	// The ampersand is an operator. Look at this variable and look at this memory address its pointing to
+
 	alexPointer := &alex
 	alexPointer.updateName("alexa")
 	alex.print()
+
+}
+
+
+func main2() {
+	jim := person2{
+		firstName: "Jim",
+		lastName: "Jimson",
+		contact: contactInfo{
+			email: "jim@jim.com",
+			zipCode: 94000,
+		},
+	}
+	fmt.Printf("%+v", jim)
 
 }
 
@@ -46,3 +76,7 @@ func (p person) print() {
 	fmt.Printf("%+v", p)
 }
 
+func main() {
+	main1()
+	main2()
+}
